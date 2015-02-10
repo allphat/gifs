31bb2d6ab785d139e4b046de1875ba4821d37212
alphatesla <ybrigant@gmail.com>
2015-02-10 17:05:58 +0100

    in progress

--- gif.sh
+++ gif.sh
@@ -19,23 +19,23 @@ then
   script_dir="$current_dir"
 fi
 
+giphy_api_key="dc6zaTOxFJmzC"
 tmp_dir="$script_dir/tmp/"
 max_files=50
 file_pattern="*.gif"
-while true; do
+#while true; do
     nb_files=`find -type f -path $tmp_dir -name $file_pattern | wc -l`
     if [ $max_files -lt $nb_files ]; then
-       echo'ooooo'
-	#find $tmp_dir -type f -amin +2 -delete
-        find $tmp_dir -type f | sort -n | cut -d' ' -f2- | head -n -2 | xargs rm  
+      find $tmp_dir -type f | sort -n | cut -d' ' -f2- | head -n -2 | xargs rm  
     fi 
-    script_dir=$(dirname $0)
-    temp_file="current.gif"
-    timestamp_date=`date +%s`
-    search_url='www.gif.tv/gifs/get.php?unique='$timestamp_date
-    gif_file=$(curl -s $search_url)
-    gif_real="$gif_file.gif"
-    curl -s "http://www.gif.tv/gifs/$gif_real" -o "$tmp_dir$gif_real"
-    mplayer -fs "$tmp_dir$gif_real" </dev/null >/dev/null 2>&1
-    sleep $((RANDOM%600+60))
-done
+
+    search_url='http://api.giphy.com/v1/gifs/trending?api_key='$giphy_api_key'&limit=1'
+    json_file=$(curl -s $search_url -o 'test.json') 
+    grep -e 'original"\:{"url":".*",' -f 'test.json'
+    #$gif_url=$json_file | sed -e 's?original":{"url":".*"?'
+    #echo $gif_url
+    #gif_real="$gif_file.gif"
+    #$url=$(curl -s "http://www.gif.tv/gifs/$gif_real" -o "$tmp_dir$gif_real")
+   # mplayer -fs "$tmp_dir$gif_real" </dev/null >/dev/null 2>&1
+   # sleep $((RANDOM%600+60))
+#done
